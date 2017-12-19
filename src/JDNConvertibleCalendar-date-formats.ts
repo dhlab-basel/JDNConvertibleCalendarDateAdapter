33b53fa29d37b04730d4fdac136d3c504b367a1f
Tobias Schweizer <t.schweizer@unibas.ch>
2017-12-19 16:56:30 +0100

    refactor (add date formats)

---
+++ src/JDNConvertibleCalendar-date-formats.ts
@@ -0,0 +1,14 @@
+import {MatDateFormats} from '@angular/material';
+
+
+export const MAT_JDN_DATE_FORMATS: MatDateFormats = {
+    parse: {
+        dateInput: 'l',
+    },
+    display: {
+        dateInput: 'l',
+        monthYearLabel: 'MMM YYYY',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'MMMM YYYY',
+    },
+};
--- src/JDNConvertibleCalendarDateAdapter.module.ts
+++ src/JDNConvertibleCalendarDateAdapter.module.ts
@@ -1,16 +1,23 @@
 import { NgModule }     from '@angular/core';
+import {
+    DateAdapter,
+    MAT_DATE_LOCALE,
+    MAT_DATE_LOCALE_PROVIDER,
+    MAT_DATE_FORMATS
+} from '@angular/material';
 import {JDNConvertibleCalendarDateAdapter} from "./JDNConvertibleCalendarDateAdapter";
-import {DateAdapter} from "@angular/material";
+import {MAT_JDN_DATE_FORMATS} from "./JDNConvertibleCalendar-date-formats";
 
 @NgModule({
     providers: [
-        {provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter/*, deps: [MAT_DATE_LOCALE]*/}
+        MAT_DATE_LOCALE_PROVIDER,
+        {provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter, deps: [MAT_DATE_LOCALE]}
     ]
 })
 export class JDNConvertibleCalendarDateAdapterModule {}
 
 @NgModule({
-    imports: [JDNConvertibleCalendarDateAdapterModule]//,
-    //providers: [{provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}],
+    imports: [JDNConvertibleCalendarDateAdapterModule],
+    providers: [{provide: MAT_DATE_FORMATS, useValue: MAT_JDN_DATE_FORMATS}],
 })
 export class MatJDNConvertibleCalendarDateAdapterModule {}
