fad39d4e78faf83023949abd6166e79d6cc07684
Tobias Schweizer <t.schweizer@unibas.ch>
2017-12-19 14:32:37 +0100

    refactor (make ngModule)

--- src/JDNConvertibleCalendarDateAdapter.module.ts
+++ src/JDNConvertibleCalendarDateAdapter.module.ts
@@ -1,17 +1,16 @@
 import { NgModule }     from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {JDNConvertibleCalendarDateAdapter} from "./JDNConvertibleCalendarDateAdapter";
 import {DateAdapter} from "@angular/material";
 
 @NgModule({
-    imports: [CommonModule],
     providers: [
-        {provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter}
+        {provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter/*, deps: [MAT_DATE_LOCALE]*/}
     ]
 })
-export class JDNConvertibleCalendarDateAdapterModule {
+export class JDNConvertibleCalendarDateAdapterModule {}
 
-
-
-
-}
+@NgModule({
+    imports: [JDNConvertibleCalendarDateAdapterModule]//,
+    //providers: [{provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}],
+})
+export class MatJDNConvertibleCalendarDateAdapterModule {}
