69dd6185995993738734fa67e24352000dab777c
Tobias Schweizer <t.schweizer@unibas.ch>
2018-03-28 18:14:06 +0200

    refactor (package.json)

--- src/app/modules/jdnconvertiblecalendardateadapter/adapter/index.ts
+++ src/app/modules/jdnconvertiblecalendardateadapter/adapter/index.ts
@@ -1,11 +1,10 @@
 import {NgModule} from '@angular/core';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MAT_DATE_LOCALE_PROVIDER} from '@angular/material';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
 import {JDNConvertibleCalendarDateAdapter} from './JDNConvertibleCalendarDateAdapter';
 import {MAT_JDN_DATE_FORMATS} from './JDNConvertibleCalendar-date-formats';
 
 @NgModule({
   providers: [
-    MAT_DATE_LOCALE_PROVIDER,
     {provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter, deps: [MAT_DATE_LOCALE]}
   ]
 })
